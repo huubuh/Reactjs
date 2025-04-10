a1392dd76119bd623eb4e6c02901f8af8d11c833
Huy Nguyen <luonghoc1011@gmail.com>
2025-04-11 06:33:37 +0700

    get api overview

--- 22667141_NguyenVanHuy_Tuan08/src/components/Overview.jsx
+++ 22667141_NguyenVanHuy_Tuan08/src/components/Overview.jsx
@@ -2,7 +2,16 @@ import Squaresfour from "../assets/img/Squares four 1.png";
 import Avatar from "../assets/img/Button 1530.png";
 import Dollar from "../assets/img/Button 1529.png";
 import Cart from "../assets/img/Button 1509.png";
+import { useEffect, useState } from "react";
 const Overview = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/overview")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+  console.log(data);
+
   return (
     <>
       <div className="flex flex-col mb-4">
@@ -12,53 +21,23 @@ const Overview = () => {
         </div>
 
         <div className="flex mx-4  space-x-6">
-          <div className="bg-pink-100 w-1/3 p-5 rounded-md">
-            <div className="flex justify-between pb-3">
-              <div>
-                <p className="font-bold">Turnover</p>
-                <span className="text-3xl font-bold">$92,405</span>
+          {data.map((item, index) => (
+            <div key={index} className={`${item.bg} w-1/3 p-5 rounded-md`}>
+              <div className="flex justify-between pb-3">
+                <div>
+                  <p className="font-bold">{item.title}</p>
+                  <span className="text-3xl font-bold">{item.value}</span>
+                </div>
+                <div>
+                  <img src={item.icon} alt="" />
+                </div>
               </div>
-              <div>
-                <img src={Cart} alt="" />
-              </div>
-            </div>
-            <div className="flex  gap-2 ">
-              <p className="text-green-600 font-bold">5.39%</p>
-              <p className="text-gray-500">period of change</p>
-            </div>
-          </div>
-
-          <div className="bg-blue-100 w-1/3 p-5 rounded-md">
-            <div className="flex justify-between pb-3">
-              <div>
-                <p className="font-bold">Profit</p>
-                <span className="text-3xl font-bold">$32,218</span>
-              </div>
-              <div>
-                <img src={Dollar} alt="" />
+              <div className="flex  gap-2 ">
+                <p className="text-green-600 font-bold">{item.change}</p>
+                <p className="text-gray-500">period of change</p>
               </div>
             </div>
-            <div className="flex  gap-2 ">
-              <p className="text-green-600 font-bold">5.39%</p>
-              <p className="text-gray-500">period of change</p>
-            </div>
-          </div>
-
-          <div className="bg-blue-50 w-1/3 p-5 rounded-md">
-            <div className="flex justify-between pb-3">
-              <div>
-                <p className="font-bold">New customer</p>
-                <span className="text-3xl font-bold">298</span>
-              </div>
-              <div>
-                <img src={Avatar} alt="" />
-              </div>
-            </div>
-            <div className="flex  gap-2 ">
-              <p className="text-green-600 font-bold">6.84%</p>
-              <p className="text-gray-500">period of change</p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
