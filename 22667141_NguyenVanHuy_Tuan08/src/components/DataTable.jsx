5da820bc8f5e31a8dee77ac2b6de2e7b2ac7b4b8
Huy Nguyen <luonghoc1011@gmail.com>
2025-04-11 02:47:33 +0700

    1_layout

--- 22667141_NguyenVanHuy_Tuan08/src/components/DataTable.jsx
+++ 22667141_NguyenVanHuy_Tuan08/src/components/DataTable.jsx
@@ -6,8 +6,8 @@ const DataTable = () => {
     <>
       <div className="flex flex-col  m-4 ">
         <div className="flex ">
-          <div className="flex gap-2 ">
-            <img src={Filetext} alt="" />
+          <div className="flex gap-2 items-center">
+            <img src={Filetext} alt="" className="w-5 h-5" />
             <p className="font-bold text-xl w-50">Detailed report</p>
           </div>
           <div className="flex ml-auto">
